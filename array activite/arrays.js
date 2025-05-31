de7f272c8fa440394eccfd0c381f2f67e045a1c1
tawnaylor <nay19001@byui.edu>
2025-05-31 12:07:17 -0600

    arrays setup

---
+++ array activite/arrays.js
@@ -0,0 +1,31 @@
+const steps = ["one", "two", "three"];
+const listTemplate(step) {
+  return `<li>${step}</li>`;
+}
+const stepsHtml = steps.map(listTemplate);
+document.querySelector("#myList").innerHTML = stepsHtml.join();
+const grades = ["A", "B", "A"];
+function convertGradeToPoints(grade) {
+  let points = 0;
+  if (grade === "A") {
+    points = 4;
+  } else if (grade === "B") {
+    points = 3;
+  }
+  return points;
+}
+const gpaPoints = grades.map(convertGradeToPoints);
+
+const gpaPoints = grades.map(convertGradeToPoints);
+const pointsTotal = gpaPoints.reduce(function (total, item) {
+  return total + item;
+});
+const gpa = pointsTotal / gpaPoints.length;
+
+// example 2
+// this is the same thing as above, but with an arrow function
+const pointsTotal = gpaPoints.reduce((total, item) => total + item);
+const gpa = pointsTotal / gpaPoints.length;
+
+// this could be further simplified as
+const gpa = gpaPoints.reduce((total, item) => total + item) / gpaPoints.length;
